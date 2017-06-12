cd96f715904ea9ea8c20cd60656fd295bfba872a
ulabspro <1sappho@gmail.com>
2017-06-12 19:01:16 +0500

    new

--- builds/static/js/main.js
+++ builds/static/js/main.js
@@ -4309,7 +4309,6 @@ $('.jsSaveFilterTrigger').click(function (e) {
   e.preventDefault();
   $(this).closest('.jsSaveFilter').find('.jsSaveFilterContent').slideToggle().find('.jsSaveFilterField').focus();
 });
-
 // Зададим стартовую дату
 // var start = new Date(),
 //     prevDay,
@@ -4351,6 +4350,7 @@ $('.jsSaveFilterTrigger').click(function (e) {
 // console.log($dp.data('datepicker').el.value);
 // // $('.datepicker-here').data('datepicker').selectDate();
 // $dp.data('datepicker').selectDate(new Date(Date.parse($dp.data('datepicker').el.value)));
+
 
 
 
@@ -4431,7 +4431,30 @@ $('.order-attention').sticky({
     bottomSpacing: 600
   }
 );
+$('.jsChangeCostTrigger').click(function (e) {
+  e.preventDefault();
+  $(this).closest('.jsChangeCost').find('.jsChangeCostArea').removeAttr('disabled').addClass('_active').focus();
+  $(this).hide();
+  $('.jsChangeCostTriggerOff').attr('data-cost', $(this).closest('.jsChangeCost').find('.jsChangeCostArea').val()).show();
+  $('.save-cost').show();
+});
 
+$('.jsChangeCostTriggerOff').click(function (e) {
+  e.preventDefault();
+  $(this).closest('.jsChangeCost').find('.jsChangeCostArea').removeClass('_active').attr('disabled', 'disabled').val($(this).data('cost'));
+  $(this).hide();
+  $('.jsChangeCostTrigger').show();
+  $('.save-cost').hide();
+});
+
+
+
+var areaSize = function() {
+  var textSize = $('.jsChangeCostArea').val().length;
+  $('.jsChangeCostArea').attr('size', textSize);
+}
+
+areaSize();
 
 
 
---
+++ markup/components/order-comments/order-comments.js
@@ -0,0 +1,24 @@
+$('.jsChangeCostTrigger').click(function (e) {
+  e.preventDefault();
+  $(this).closest('.jsChangeCost').find('.jsChangeCostArea').removeAttr('disabled').addClass('_active').focus();
+  $(this).hide();
+  $('.jsChangeCostTriggerOff').attr('data-cost', $(this).closest('.jsChangeCost').find('.jsChangeCostArea').val()).show();
+  $('.save-cost').show();
+});
+
+$('.jsChangeCostTriggerOff').click(function (e) {
+  e.preventDefault();
+  $(this).closest('.jsChangeCost').find('.jsChangeCostArea').removeClass('_active').attr('disabled', 'disabled').val($(this).data('cost'));
+  $(this).hide();
+  $('.jsChangeCostTrigger').show();
+  $('.save-cost').hide();
+});
+
+
+
+var areaSize = function() {
+  var textSize = $('.jsChangeCostArea').val().length;
+  $('.jsChangeCostArea').attr('size', textSize);
+}
+
+areaSize();
